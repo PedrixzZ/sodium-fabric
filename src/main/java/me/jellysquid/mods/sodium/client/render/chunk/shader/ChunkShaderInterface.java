e83b6c988ceb7b4c66980ba691e61fa48846a96e
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-06-23 12:05:34 -0300

    Update ChunkShaderInterface.java

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkShaderInterface.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkShaderInterface.java
@@ -1,10 +1,12 @@
 package me.jellysquid.mods.sodium.client.render.chunk.shader;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import me.jellysquid.mods.sodium.client.gl.shader.uniform.GlUniformFloat3v;
 import me.jellysquid.mods.sodium.client.gl.shader.uniform.GlUniformInt;
 import me.jellysquid.mods.sodium.client.gl.shader.uniform.GlUniformMatrix4f;
 import me.jellysquid.mods.sodium.client.util.TextureUtil;
 import org.joml.Matrix4fc;
+import org.lwjgl.opengl.GL32C;
 
 import java.util.EnumMap;
 import java.util.Map;
@@ -19,6 +21,7 @@ public class ChunkShaderInterface {
     private final GlUniformMatrix4f uniformProjectionMatrix;
     private final GlUniformFloat3v uniformRegionOffset;
 
+    // The fog shader component used by this program in order to setup the appropriate GL state
     private final ChunkShaderFogComponent fogShader;
 
     public ChunkShaderInterface(ShaderBindingContext context, ChunkShaderOptions options) {
@@ -33,6 +36,23 @@ public class ChunkShaderInterface {
         this.fogShader = options.fog().getFactory().apply(context);
     }
 
+    @Deprecated // the shader interface should not modify pipeline state
+    public void setupState() {
+        this.bindTexture(ChunkShaderTextureSlot.BLOCK, TextureUtil.getBlockTextureId());
+        this.bindTexture(ChunkShaderTextureSlot.LIGHT, TextureUtil.getLightTextureId());
+
+        this.fogShader.setup();
+    }
+
+    @Deprecated(forRemoval = true) // should be handled properly in GFX instead.
+    private void bindTexture(ChunkShaderTextureSlot slot, int textureId) {
+        GlStateManager._activeTexture(GL32C.GL_TEXTURE0 + slot.ordinal());
+        GlStateManager._bindTexture(textureId);
+
+        var uniform = this.uniformTextures.get(slot);
+        uniform.setInt(slot.ordinal());
+    }
+
     public void setProjectionMatrix(Matrix4fc matrix) {
         this.uniformProjectionMatrix.set(matrix);
     }
@@ -43,21 +63,5 @@ public class ChunkShaderInterface {
 
     public void setRegionOffset(float x, float y, float z) {
         this.uniformRegionOffset.set(x, y, z);
-    }
-
-    // Método para configurar o estado, não mais modificando o estado do pipeline diretamente
-    public void configureState() {
-        // Exemplo de configuração de estado como era feito em setupState()
-        // Evitar GLStateManager e a manipulação direta de GL aqui
-        this.bindTexture(ChunkShaderTextureSlot.BLOCK, TextureUtil.getBlockTextureId());
-        this.bindTexture(ChunkShaderTextureSlot.LIGHT, TextureUtil.getLightTextureId());
-
-        this.fogShader.setup();
-    }
-
-    // Método privado para vincular texturas - agora mais simplificado
-    private void bindTexture(ChunkShaderTextureSlot slot, int textureId) {
-        var uniform = this.uniformTextures.get(slot);
-        uniform.setInt(slot.ordinal());
     }
 }
